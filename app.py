0e4eaf380040ddbbbf0ec5c828b5c0fb146d29d4
Blaine Oler <olerblaine@gmail.com>
2024-07-17 17:15:55 -0400

    main

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+import json
 
-app = Flask(__name__)
+url = 'https://scores.bowl.com/2024-JG/Qualifying_Round%201_U18Boys.pdf?v=new'  # Replace with your target URL
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+data = soup.find_all('p')
+results = [item.get_text() for item in data]
 
-@app.route('/scrape')
-def scrape():
-    url = 'https://scores.bowl.com/2024-JG/Qualifying_Round%201_U18Boys.pdf?v=new'  # Replace with your target URL
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    data = soup.find_all('p')
-    results = [item.get_text() for item in data]
-    return jsonify(results)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+with open('data.json', 'w') as f:
+    json.dump(results, f)
